6215fb231145368aa3c451356339f72c6a0f521d
smr <smr20thi@gmail.com>
2025-10-05 12:57:44 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyAUj9w89BSmzO7QIlU5w6fFzB-_FR_tktw",
   authDomain: "women-safest-route.firebaseapp.com",
@@ -11,3 +12,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
