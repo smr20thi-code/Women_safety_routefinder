4a8a6538516998477b4a3058640219c867382097
smr <smr20thi@gmail.com>
2025-10-05 12:33:35 +0530

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyAUj9w89BSmzO7QIlU5w6fFzB-_FR_tktw",
+  authDomain: "women-safest-route.firebaseapp.com",
+  databaseURL: "https://women-safest-route-default-rtdb.firebaseio.com",
+  projectId: "women-safest-route",
+  storageBucket: "women-safest-route.firebasestorage.app",
+  messagingSenderId: "900494947297",
+  appId: "1:900494947297:web:f832c14cf960069efd58a0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
