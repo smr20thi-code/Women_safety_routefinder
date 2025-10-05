070cb56441279c57799d288ae51c02d6583861c2
smr <smr20thi@gmail.com>
2025-10-05 11:42:01 +0530

    Update service-worker.js

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,7 +1,17 @@
 /* eslint-disable no-restricted-globals */
+import { clientsClaim } from 'workbox-core';
+import { precacheAndRoute } from 'workbox-precaching';
+
+clientsClaim();
+self.skipWaiting();
+
+
+precacheAndRoute(self.__WB_MANIFEST || []);
+
 const CACHE_NAME = "safety-route-cache-v1";
 const urlsToCache = ["/", "/index.html", "/manifest.json"];
 
+// Cache extra URLs during install
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -10,10 +20,27 @@ self.addEventListener("install", (event) => {
   );
 });
 
+
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
+    })
+  );
+});
+
+
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
